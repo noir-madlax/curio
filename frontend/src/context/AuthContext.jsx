f50e0515b45d7c8d593850f52ddf6f57c1f6854d
zhangzic <zhangzic@gmail.com>
2025-04-25 18:37:03 +0800

    修复sso登陆

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -54,7 +54,7 @@ export const AuthProvider = ({ children }) => {
       supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: window.location.origin + '/surveys',
         }
       }),
       
